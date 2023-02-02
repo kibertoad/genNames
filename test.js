9abb8987a8455598ab34ae73838f8e57a756e0e7
Igor Savin <iselwin@gmail.com>
2023-02-02 03:11:36 +0200

    First commit

--- test.js
+++ test.js
@@ -1,36 +1,34 @@
-var genNames = require('./index.js');
+var genNames = require('./index.js')
 
 var options = {
   count: 10,
   gender: 'male',
-  surname: true
-};
+  surname: true,
+}
 
-genNames(options,function(names) {
-  console.log("\nMales\n");
-  console.log(names);
-});
-
+genNames(options, function (names) {
+  console.log('\nMales\n')
+  console.log(names)
+})
 
 var options = {
   count: 10,
   gender: 'females',
-  surname: true
-};
+  surname: true,
+}
 
-genNames(options,function(names) {
-  console.log('\nFemales\n');
-  console.log(names);
-});
+genNames(options, function (names) {
+  console.log('\nFemales\n')
+  console.log(names)
+})
 
 var options = {
   count: 10,
   gender: false,
-  surname: false
-};
+  surname: false,
+}
 
-genNames(options,function(names) {
-  console.log('\nRandom Gender with no Last Names\n');
-  console.log(names);
-});
-
+genNames(options, function (names) {
+  console.log('\nRandom Gender with no Last Names\n')
+  console.log(names)
+})
